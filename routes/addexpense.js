cf1d889fd84bc12980bd26916dbc8b4d18f4515e
adityasinha198 <adityasinha198@gmail.com>
2022-09-22 21:23:43 +0530

    task7routes

--- routes/addexpense.js
+++ routes/addexpense.js
@@ -12,5 +12,5 @@ router.post('/addexpense', addexpenseController.postaddexpense);
 router.get('/getexpense',userauthentication.authenticate ,addexpenseController.getaddexpense)
 router.delete('/deleteexpense/:Id',addexpenseController.deleteExpenses)
 
-
+//
 module.exports = router 
